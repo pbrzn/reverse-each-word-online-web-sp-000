9e9b2f159ba6f70ec557b6b6419f038406d62cc9
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-17 15:02:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 def reverse_each_word(string)
   output=[]
   new_string=string.split(" ")
-  new_string.each do |word|
+  new_string.collect do |word|
     output << word.reverse
   end
   output.join(" ")
 end
+
